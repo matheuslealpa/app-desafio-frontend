de02da70b979011474c7ee393f41d7c9531610da
Matheus Leal <matheuslealpa@gmail.com>
2024-02-27 22:42:47 -0300

    feat: desafio frontend vflow

---
+++ README.md
@@ -0,0 +1,24 @@
+# APP-DESAFIO-FRONTEND
+
+Este é o projeto frontend para o desafio VFLOWS/Teste-Estagio. O objetivo deste desafio é avaliar meus conhecimentos técnicos, como lido com os requisitos e a capacidade de pensar em soluções.
+
+## Instalação
+
+Certifique-se de ter o Node.js 18.17.0 instalado em sua máquina. Em seguida, execute o seguinte comando para instalar as dependências do projeto:
+
+```bash
+npm i
+```
+
+Isso instalará as seguintes dependências:
+ - Bootstrap 5.3.3
+ - jQuery 3.5.1
+
+Basta inicializar o arquivo app-desafio-frontend/src/index.html em seu navegador para verificar 
+
+## Outras Dependências
+O arquivo index.html também inclui os seguintes recursos:
+
+ - Bootstrap 4.5.2: Framework CSS para estilização e layout responsivo.
+ - Font Awesome 5.15.4: Conjunto de ícones para uso em projetos web.
+ - Fluig Style Guide: Guia de estilo do Fluig.
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    // Evento ao perder o foco do campo CEP
     $('#cep').blur(function () {
         var cep = $(this).val().replace(/\D/g, '');
 
@@ -7,6 +8,7 @@ $(document).ready(function () {
             return;
         }
 
+        // Requisição para obter dados do CEP
         $.getJSON('https://viacep.com.br/ws/' + cep + '/json/', function (data) {
             if (!("erro" in data)) {
                 $('#endereco').val(data.logradouro);
@@ -21,34 +23,145 @@ $(document).ready(function () {
     });
 });
 
+// Evento ao clicar no botão para adicionar anexo
+$(document).ready(function () {
+    $('#adicionarAnexo').click(function () {
+        $('#fileInput').click(); // Simular clique no input de arquivo
+    });
+
+    // Manipular mudança no input de arquivo
+    $('#fileInput').change(function () {
+        var file = this.files[0]; // Obter o arquivo selecionado
+        if (file) {
+            // Armazenar o arquivo no localStorage
+            var reader = new FileReader();
+            reader.onload = function (event) {
+                var fileData = event.target.result;
+                localStorage.setItem('anexo', fileData);
+                adicionarAnexo(file.name);
+            };
+            reader.readAsDataURL(file);
+        }
+    });
+
+    function adicionarAnexo(fileName) {
+        var anexoHtml = `
+            <div class="anexo">
+                <i class="far fa-eye visualizar-anexo" title="Visualizar Anexo"></i>
+                <i class="far fa-trash-alt excluir-anexo" title="Excluir Anexo"></i>
+                <span>${fileName}</span>
+            </div>
+        `;
+        $('#anexos').append(anexoHtml);
+    }
+
+    $('#anexos').on('click', '.excluir-anexo', function () {
+        localStorage.removeItem('anexo');
+        $(this).closest('.anexo').remove();
+    });
+
+    $('#anexos').on('click', '.visualizar-anexo', function () {
+        var fileData = localStorage.getItem('anexo');
+        if (fileData) {
+            alert("Simulando a visualização do arquivo: " + fileData);
+        }
+    });
+});
+
+$(document).ready(function () {
+    $('#salvarFornecedor').click(function () {
+        salvarFornecedor();
+    });
+});
+
+
+function validarCamposObrigatorios() {
+    var camposObrigatoriosPreenchidos = true;
+    $('.required').each(function () {
+        if ($(this).val().trim() === '') {
+            camposObrigatoriosPreenchidos = false;
+            alert('Por favor, preencha todos os campos obrigatórios.');
+            return false; // Sair do loop se algum campo estiver vazio
+        }
+    });
+
+    return camposObrigatoriosPreenchidos;
+}
+
+
+function salvarFornecedor() {
+    var fornecedor = {
+        razaoSocial: $('#razaoSocial').val(),
+        nomeFantasia: $('#nomeFantasia').val(),
+        cnpj: $('#cnpj').val(),
+        inscricaoEstadual: $('#inscricaoEstadual').val(),
+        inscricaoMunicipal: $('#inscricaoMunicipal').val(),
+        nomeContato: $('#pessoaContato').val(),
+        telefoneContato: $('#telefone').val(),
+        emailContato: $('#email').val(),
+        produtos: [],
+        anexos: []
+    };
+
+    // Obter produtos
+    $('.produto').each(function (index) {
+        var produto = {
+            indice: index + 1,
+            descricaoProduto: $(this).find('input[name="descricao"]').val(),
+            unidadeMedida: $(this).find('select[name="unidadeMedida"]').val(),
+            qtdeEstoque: $(this).find('input[name="quantidadeEstoque"]').val(),
+            valorUnitario: $(this).find('input[name="valorUnitario"]').val(),
+            valorTotal: $(this).find('input[name="valorTotal"]').val()
+        };
+        fornecedor.produtos.push(produto);
+    });
+
+    // Obter anexos
+    $('.anexo').each(function (index) {
+        var anexo = {
+            indice: index + 1,
+            nomeArquivo: $(this).find('span').text(),
+            blobArquivo: localStorage.getItem('anexo')
+        };
+        fornecedor.anexos.push(anexo);
+    });
+    console.log(JSON.stringify(fornecedor, null, 4));
+}
+
+
 $(document).ready(function () {
     // Função para adicionar um produto
     function adicionarProduto() {
         var produtoHtml = `
             <div class="form-row produto border">
                 <div class="form-group col-md-12">
-                    <label for="descricao">Descrição<span class="text-danger">*</span>:</label>
+                    <label for="descricao">Descrição<span class="text-danger required">*</span>:</label>
                     <input type="text" class="form-control" name="descricao" required>
+                    <span class="error-message">Este campo é obrigatório.</span>
                 </div>
                 <div class="form-group col-md-3">
-                    <label for="unidadeMedida">Unidade de Medida<span class="text-danger">*</span>:</label>
+                    <label for="unidadeMedida">Unidade de Medida<span class="text-danger required">*</span>:</label>
                     <select class="form-control" name="unidadeMedida" required>
                         <option value="">Selecione...</option>
                         <option value="un">Unidade</option>
                         <option value="kg">Quilograma</option>
                     </select>
+                    <span class="error-message">Este campo é obrigatório.</span>
                 </div>
                 <div class="form-group col-md-3">
-                    <label for="quantidadeEstoque">Quantidade em Estoque<span class="text-danger">*</span>:</label>
+                    <label for="quantidadeEstoque">Quantidade em Estoque<span class="text-danger required">*</span>:</label>
                     <input type="number" class="form-control" name="quantidadeEstoque" value="0" required>
-                </div>
+                    <span class="error-message">Este campo é obrigatório.</span>
+                    </div>
                 <div class="form-group col-md-3">
-                    <label for="valorUnitario">Valor Unitário<span class="text-danger">*</span>:</label>
+                    <label for="valorUnitario">Valor Unitário<span class="text-danger required">*</span>:</label>
                     <input type="number" class="form-control" name="valorUnitario" value="0" required>
+                    <span class="error-message">Este campo é obrigatório.</span>
                 </div>
                 <div class="form-group col-md-3">
                     <label for="valorTotal">Valor Total<span class="text-danger">*</span>:</label>
                     <input type="text" class="form-control" name="valorTotal" readonly>
+                    
                 </div>
                 <i class="far fa-trash-alt delete-icon" title="Excluir Produto"></i>
             </div>
@@ -83,3 +196,27 @@ $(document).ready(function () {
         }
     });
 });
+
+
+$(document).ready(function() {
+    $('#salvarFornecedor').click(function() {
+      // Abrir o modal de loading
+      $('#loadingModal').modal('show');
+      
+      // Formatar os dados em JSON e enviar
+      enviarDados();
+    });
+  });
+  
+  function enviarDados() {
+    // Formatar os dados em JSON
+    var dados = salvarFornecedor();
+  
+    setTimeout(function() {
+      $('#loadingModal').modal('hide');
+      
+      alert('Dados enviados com sucesso!');
+    }, 2000);
+  }
+  
+  
